0b5d87f928746c62fd25d9601f7b4a4fb7d30e4f
aapiane09 <aapiane09@gmail.com>
2016-12-24 23:43:22 -0800

    Tests form in modal

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 console.log("DOM Ready!");
 
+//Initialize Form Modal
   $(".btn").click(function (){
     console.log("Button clicked!");
     $('#modal1').modal();
   })
 
+//Form Option Select
+  $('select').material_select();
 
 
 });
