b0799e03b7bde09b9ec1eac6ca97f1056c93594f
aapiane09 <aapiane09@gmail.com>
2016-12-24 21:23:06 -0800

    Creates neighborhoods page, tests page design elements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
-     $('.carousel').carousel();
-     $('.carousel.carousel-slider').carousel({full_width: true});
-     // Next slide
-     $('.carousel').carousel('next');
-     $('.carousel').carousel('next', 3); // Move next n times.
-     // Previous slide
-     $('.carousel').carousel('prev');
-     $('.carousel').carousel('prev', 4); // Move prev n times.
-     // Set to nth slide
-     $('.carousel').carousel('set', 4);
+
    });
