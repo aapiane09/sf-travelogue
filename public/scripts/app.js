ef457ab6a3ebf1d4ebae369f0564c0bdb4004946
greicens <gsilva@mail.ccsf.edu>
2016-12-26 16:22:37 -0800

    updates modals description

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-console.log("DOM Ready!");
+  console.log("DOM Ready!");
 
 //Initialize Form Modal
   $(".btn").click(function (){
@@ -10,5 +10,28 @@ console.log("DOM Ready!");
 //Form Option Select
   $('select').material_select();
 
+//Modal footer add button
 
+//   $('.modal-action').on('click', function(target){
+//     target.preventDefault();
+//       console.log('new place serialized', $(this).serializeArray());
+//       $.ajax({
+//         method: 'POST',
+//         url: 'api/neighborhoods/:hoodId/places',
+//         data: $(this).serializeArray(),
+//         success: newPlaceSuccess,
+//         error: newPlaceError
+//       });
+//   });
 });
+
+// function newPlaceSuccess(json){
+//   $('#newProjetForm input').val('');
+//   allProjects.push(json);//what does this do?
+//   console.log("new project created", json);
+//   render();
+// }
+//
+// function newPlaceError(){
+//   console.log('new project error!');
+// }
