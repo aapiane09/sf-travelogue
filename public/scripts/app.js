89097c1d01b1a752f5089cc0e0a443e109667d6f
aapiane09 <aapiane09@gmail.com>
2016-12-29 10:03:28 -0800

    Fixes hoisting issue with carousel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,9 @@ var allNeighborhoods = [];
 $(document).ready(function(){
   console.log("DOM Ready!");
 
+  //Initialize Slider
+  $('.carousel.carousel-slider').carousel({full_width: true});
+  
   //Form Option Select
   $('select').material_select();
 
@@ -18,7 +21,32 @@ $(document).ready(function(){
     url: '/api/neighborhoods',
     success: onSuccess,
     error: onError
-    
+  });
+
+
+// //Initialize Form Modal
+//   $(".btn").click(function (){
+//     console.log("Button clicked!");
+//     $('#modal1').modal();
+//   })
+
+//Form Option Select
+  $('select').material_select();
+
+
+//Modal footer add button
+
+//   $('.modal-action').on('click', function(target){
+//     target.preventDefault();
+//       console.log('new place serialized', $(this).serializeArray());
+//       $.ajax({
+//         method: 'POST',
+//         url: 'api/neighborhoods/:hoodId/places',
+//         data: $(this).serializeArray(),
+//         success: newPlaceSuccess,
+//         error: newPlaceError
+//       });
+//   });
 });
 
 
