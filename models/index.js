259a3095ea5cba5df4ab93d59d8a3a9e5eccc241
aapiane09 <aapiane09@gmail.com>
2017-01-02 15:08:38 -0800

    Changes mongoose URL

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/gsilva");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/sf-travelogue");
 
 module.exports.Neighborhood = require("./neighborhood.js");
 module.exports.Place = require("./place.js");
